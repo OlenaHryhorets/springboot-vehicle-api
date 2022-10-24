fd85d7b2c89b56ab2d8a797882dfc16503d56cbc
Olena Hryhorets <ohryhore@jaguarlandrover.com>
2022-10-24 21:16:05 +0100

    init

--- src/main/java/org/mystudy/Main.java
+++ src/main/java/org/mystudy/Main.java
@@ -1,4 +1,6 @@
-package org.mystudy;public class Main {
+package org.mystudy;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
