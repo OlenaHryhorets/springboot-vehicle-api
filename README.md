31c5e75ca3876db2da2f0eb3c9a0a7c93e77a61c
Olena Hryhorets <adamenkolena7@gmail.com>
2022-11-11 14:16:59 +0000

    replaced getters and setters with lombok.Data annotation

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ WIP:
   - replaced springdoc-openapi-ui dependency (provide an open API in the path ex. springdoc.api-docs.path=/api/v1/docs) 
 with using the Swagger UI to dynamically generate beautiful documentation for API and host it with GitHub Pages 
 (reference: [link](https://github.com/peter-evans/swagger-github-pages)) 
-  - replaced default and all arguments constructors with lombok annotations
+  - replaced default and all arguments constructors, getters and setters with lombok annotations
--- src/main/java/com/myorg/model/Vehicle.java
+++ src/main/java/com/myorg/model/Vehicle.java
@@ -1,18 +1,19 @@
 package com.myorg.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 @Entity
 @Table(name = "vehicle")
 public class Vehicle {
+  @Id
   private int id;
   private String title;
   private String model;
@@ -20,63 +21,4 @@ public class Vehicle {
   private int millage;
   private String color;
   private String registrationCountry;
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getModel() {
-    return model;
-  }
-
-  public void setModel(String model) {
-    this.model = model;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public int getMillage() {
-    return millage;
-  }
-
-  public void setMillage(int millage) {
-    this.millage = millage;
-  }
-
-  public String getColor() {
-    return color;
-  }
-
-  public void setColor(String color) {
-    this.color = color;
-  }
-
-  public String getRegistrationCountry() {
-    return registrationCountry;
-  }
-
-  public void setRegistrationCountry(String registrationCountry) {
-    this.registrationCountry = registrationCountry;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  public int getId() {
-    return id;
-  }
-
 }
